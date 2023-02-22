a83fbacf29d42501d7e2ae8e595df093db7ac17b
vrammoorthi <123871259+vrammoorthi@users.noreply.github.com>
2023-02-22 10:51:04 +0530

    Add files via upload

---
+++ Website Fullstack/web.js
@@ -0,0 +1,23 @@
+function ram(){
+   alert("Create Account First");
+}
+
+setTimeout(() => { document.title="Hello"},1000);
+setTimeout(() => { document.title="welcome"},2000);
+setTimeout(() => { document.title="Ram"},3000);
+
+// function color(){
+//     document.body.style.background="#1e1e85";
+//    let element=document.querySelectorAll("#para");
+//    for(let i=0;i<element.length;i++){
+//     element[i].style.color="white";
+//    }
+// }
+
+//     setInterval(() => { document.body.style.background="#000a40";},1000);
+// setInterval(() => { document.body.style.background="#1e1e85";},2000);
+// setInterval(() => { document.body.style.background="#000a40";},3000);
+// // setInterval(() => { document.body.style.background="#1e1e85";},4000);
+// // setInterval(() => { document.body.style.background="#000a40";},5000);
+// // setInterval(() => { document.body.style.background="#1e1e85";},6000);
+// // setInterval(() => { document.body.style.background="#000a40";},7000);
